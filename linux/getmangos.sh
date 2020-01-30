2e606601bb3db587d7a6d449508d34f6a80833af
GGK <blackstao@gmail.com>
2020-01-30 23:49:37 +0800

    Update getmangos.sh
    Fix indentation.

--- linux/getmangos.sh
+++ linux/getmangos.sh
@@ -256,15 +256,15 @@ function GetPrerequisites()
           # Ubuntu 17.10
           su -c "apt-get -y install build-essential curl autoconf automake cmake libbz2-dev libace-dev libssl-dev libmysqlclient-dev libtool" root
           ;;
-          "bionic")
+        "bionic")
           # Ubuntu 18.04 LTS
           su -c "apt-get -y install build-essential curl autoconf automake cmake libbz2-dev libace-dev libssl-dev libmysqlclient-dev libtool" root
           ;;
-          "disco")
+        "disco")
           # Ubuntu 19.04
           su -c "apt-get -y install build-essential curl autoconf automake cmake libbz2-dev libace-dev libssl-dev libmysqlclient-dev libtool" root
           ;;
-          *)
+        *)
           OS_VER=0
           ;;
       esac
